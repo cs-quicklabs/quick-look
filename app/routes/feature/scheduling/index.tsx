c52717cea829b9a35cb8d3a4a3a48997af22b151
kunalvemra0412 <kverma829@gmail.com>
2022-06-15 13:56:19 +0530

    added feature page sections

---
+++ app/routes/feature/scheduling/index.tsx
@@ -0,0 +1,19 @@
+export default function Signature() {
+  return (
+    <div className='flex flex-col gap-12 mt-10'>
+      <div>
+        <h1 className=' text-center object-cover text-xl'>
+          Let people book meetings with you right from your page
+          <br /> when you connect your Google account.
+        </h1>
+      </div>
+      <div className='flex justify-center  items-center'>
+        <img
+          src='https://about.me/s3/h/z/appointment_scheduling.226967d9.jpg'
+          alt=''
+          className='lg:w-2/4 object-cover'
+        />
+      </div>
+    </div>
+  )
+}
---
+++ app/routes/feature/visitmywebsite/index.tsx
@@ -0,0 +1,19 @@
+export default function Signature() {
+  return (
+    <div className='flex flex-col gap-12 mt-10'>
+      <div>
+        <h1 className=' text-center object-cover text-xl'>
+          Get more visits to your portfolio, blog, photos —
+          <br /> anywhere you’d like to point people to.
+        </h1>
+      </div>
+      <div className='flex justify-center  items-center'>
+        <img
+          src='https://about.me/s3/h/z/visit_website.6696e4b6.jpg'
+          alt=''
+          className='lg:w-2/4 object-cover'
+        />
+      </div>
+    </div>
+  )
+}
