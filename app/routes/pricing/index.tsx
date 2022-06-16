54537f6b69d9ddd44a9bcfe3f9a7fbd13713836b
diikshaaaa <diksha.grover@crownstack.com>
2022-06-16 12:42:29 +0530

    pricing page

---
+++ app/routes/pricing/index.tsx
@@ -0,0 +1,195 @@
+export default function Pricing() {
+  return (
+  <>
+    <div className="bg-white">
+        <div className="bg-gray-800">
+            <div className="pt-12 sm:pt-16 lg:pt-24">
+                <div className="max-w-7xl mx-auto text-center px-4 sm:px-6 lg:px-8">
+                    <div className="max-w-3xl mx-auto space-y-2 lg:max-w-none">
+                        <h2 className="text-5xl leading-6 font-bold text-gray-300 uppercase tracking-wider pb-3">Pricing</h2>
+                        <p className="text-lg font-extrabold text-white sm:text-4xl lg:text-5xl">The right price for you, whoever you are</p>
+                        <p className="text-xl text-gray-300">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum sequi unde repudiandae natus.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="mt-8 pb-12 bg-white sm:mt-12 sm:pb-16 lg:mt-16 lg:pb-24">
+                <div className="relative">
+                    <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                        <div className="max-w-md mx-auto space-y-4 lg:max-w-5xl lg:grid lg:grid-cols-2 lg:gap-5 lg:space-y-0">
+                            <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
+                                <div className="px-6 py-8 bg-white sm:p-10 sm:pb-6">
+                                    <div>
+                                        <h3 className="inline-flex px-4 py-1 rounded-full text-sm font-semibold tracking-wide uppercase bg-indigo-100 text-indigo-600" id="tier-standard">Standard</h3>
+                                    </div>
+                                    <div className="mt-4 flex items-baseline text-6xl font-extrabold">
+                                        $49
+                                        <span className="ml-1 text-2xl font-medium text-gray-500"> /mo </span>
+                                    </div>
+                                    <p className="mt-5 text-lg text-gray-500">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+                                </div>
+                                <div className="flex-1 flex flex-col justify-between px-6 pt-6 pb-8 bg-gray-50 space-y-6 sm:p-10 sm:pt-6">
+                                    <ul role="list" className="space-y-4">
+                                        <li className="flex items-start">
+                                            <div className="flex-shrink-0">    
+                                                <svg className="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
+                                                </svg>
+                                            </div>
+                                            <p className="ml-3 text-base text-gray-700">Pariatur quod similique</p>
+                                        </li>
+                                        <li className="flex items-start">
+                                            <div className="flex-shrink-0">
+                                                <svg className="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
+                                                </svg>
+                                            </div>
+                                            <p className="ml-3 text-base text-gray-700">Sapiente libero doloribus modi nostrum</p>
+                                        </li>
+                                        <li className="flex items-start">
+                                            <div className="flex-shrink-0">
+                                                <svg className="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
+                                                </svg>
+                                            </div>
+                                            <p className="ml-3 text-base text-gray-700">Vel ipsa esse repudiandae excepturi</p>
+                                        </li>
+                                        <li className="flex items-start">
+                                            <div className="flex-shrink-0">
+                                                <svg className="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
+                                                </svg>
+                                            </div>
+                                            <p className="ml-3 text-base text-gray-700">Itaque cupiditate adipisci quibusdam</p>
+                                        </li>
+                                    </ul>
+                                    <div className="rounded-md shadow">
+                                        <a href="#" className="flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gray-800 hover:bg-gray-600" aria-describedby="tier-standard"> Get started </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
+                                <div className="px-6 py-8 bg-white sm:p-10 sm:pb-6">
+                                    <div>
+                                        <h3 className="inline-flex px-4 py-1 rounded-full text-sm font-semibold tracking-wide uppercase bg-indigo-100 text-indigo-600" id="tier-standard">Enterprise</h3>
+                                    </div>
+                                    <div className="mt-4 flex items-baseline text-6xl font-extrabold">
+                                        $79
+                                        <span className="ml-1 text-2xl font-medium text-gray-500"> /mo </span>
+                                    </div>
+                                    <p className="mt-5 text-lg text-gray-500">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+                                </div>
+                                <div className="flex-1 flex flex-col justify-between px-6 pt-6 pb-8 bg-gray-50 space-y-6 sm:p-10 sm:pt-6">
+                                    <ul role="list" className="space-y-4">
+                                        <li className="flex items-start">
+                                            <div className="flex-shrink-0">
+                                                <svg className="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
+                                                </svg>
+                                            </div>
+                                            <p className="ml-3 text-base text-gray-700">Pariatur quod similique</p>
+                                        </li>
+                                        <li className="flex items-start">
+                                            <div className="flex-shrink-0">  
+                                                <svg className="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
+                                                </svg>
+                                            </div>
+                                            <p className="ml-3 text-base text-gray-700">Sapiente libero doloribus modi nostrum</p>
+                                        </li>
+                                        <li className="flex items-start">
+                                            <div className="flex-shrink-0">
+                                                <svg className="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
+                                                </svg>
+                                            </div>
+                                            <p className="ml-3 text-base text-gray-700">Vel ipsa esse repudiandae excepturi</p>
+                                        </li>
+                                        <li className="flex items-start">
+                                            <div className="flex-shrink-0">  
+                                                <svg className="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
+                                                </svg>
+                                            </div>
+                                            <p className="ml-3 text-base text-gray-700">Itaque cupiditate adipisci quibusdam</p>
+                                        </li>
+                                    </ul>
+                                    <div className="rounded-md shadow">
+                                        <a href="#" className="flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gray-800 hover:bg-gray-600" aria-describedby="tier-standard"> Get started </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="mt-4 relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 lg:mt-5">
+                    <div className="max-w-md mx-auto lg:max-w-5xl">
+                        <div className="rounded-lg bg-gray-100 px-6 py-8 sm:p-10 lg:flex lg:items-center">
+                            <div className="flex-1">
+                                <div>
+                                    <h3 className="inline-flex px-4 py-1 rounded-full text-sm font-semibold tracking-wide uppercase bg-white text-gray-800">Discounted</h3>
+                                </div>
+                                <div className="mt-4 text-lg text-gray-600">Get full access to all of standard license features for solo projects that make less than $20k gross revenue for <span className="font-semibold text-gray-900">$29</span>.
+                                </div>
+                            </div>
+                            <div className="mt-6 rounded-md shadow lg:mt-0 lg:ml-10 lg:flex-shrink-0">
+                                <a href="#" className="flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-gray-900 bg-white hover:bg-gray-50"> Buy Discounted License </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="absolute top-3/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <button className="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
+                    Get Your free page
+            </button>
+        </div>
+        <div className="bg-white mt-5">
+            <div className="max-w-7xl mx-auto py-12 px-4 divide-y divide-gray-200 sm:px-6 lg:py-16 lg:px-8">
+                <h2 className="text-3xl font-extrabold text-gray-900">Frequently asked questions</h2>
+                <div className="mt-8">
+                    <dl className="divide-y divide-gray-200">
+                        <div className="pt-6 pb-8 md:grid md:grid-cols-12 md:gap-8">
+                            <dt className="text-base font-medium text-gray-900 md:col-span-5">What&#039;s the best thing about Switzerland?</dt>
+                            <dd className="mt-2 md:mt-0 md:col-span-7">
+                                <p className="text-base text-gray-500">I don&#039;t know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.</p>
+                            </dd>
+                        </div>
+                        <div className="pt-6 pb-8 md:grid md:grid-cols-12 md:gap-8">
+                            <dt className="text-base font-medium text-gray-900 md:col-span-5">Lorem ipsum dolor sit amet consectetur, adipisicing elit?</dt>
+                            <dd className="mt-2 md:mt-0 md:col-span-7">
+                                <p className="text-base text-gray-500">Quod doloribus nemo maxime similique natus maiores omnis recusandae accusantium nulla cupiditate sit exercitationem eos nam reiciendis molestiae, odio ipsa saepe ducimus.</p>
+                            </dd>
+                        </div>
+                        <div className="pt-6 pb-8 md:grid md:grid-cols-12 md:gap-8">
+                            <dt className="text-base font-medium text-gray-900 md:col-span-5">Lorem ipsum dolor sit amet consectetur, adipisicing elit?</dt>
+                            <dd className="mt-2 md:mt-0 md:col-span-7">
+                                <p className="text-base text-gray-500">Quod doloribus nemo maxime similique natus maiores omnis recusandae accusantium nulla cupiditate sit exercitationem eos nam reiciendis molestiae, odio ipsa saepe ducimus.</p>
+                            </dd>
+                        </div>
+                        <div className="pt-6 pb-8 md:grid md:grid-cols-12 md:gap-8">
+                            <dt className="text-base font-medium text-gray-900 md:col-span-5">Lorem ipsum dolor sit amet consectetur, adipisicing elit?</dt>
+                            <dd className="mt-2 md:mt-0 md:col-span-7">
+                                <p className="text-base text-gray-500">Quod doloribus nemo maxime similique natus maiores omnis recusandae accusantium nulla cupiditate sit exercitationem eos nam reiciendis molestiae, odio ipsa saepe ducimus.</p>
+                            </dd>
+                        </div>
+                        <div className="pt-6 pb-8 md:grid md:grid-cols-12 md:gap-8">
+                            <dt className="text-base font-medium text-gray-900 md:col-span-5">Lorem ipsum dolor sit amet consectetur, adipisicing elit?</dt>
+                            <dd className="mt-2 md:mt-0 md:col-span-7">
+                                <p className="text-base text-gray-500">Quod doloribus nemo maxime similique natus maiores omnis recusandae accusantium nulla cupiditate sit exercitationem eos nam reiciendis molestiae, odio ipsa saepe ducimus.</p>
+                            </dd>
+                        </div>
+                        <div className="pt-6 pb-8 md:grid md:grid-cols-12 md:gap-8">
+                            <dt className="text-base font-medium text-gray-900 md:col-span-5">Lorem ipsum dolor sit amet consectetur, adipisicing elit?</dt>
+                            <dd className="mt-2 md:mt-0 md:col-span-7">
+                                <p className="text-base text-gray-500">Quod doloribus nemo maxime similique natus maiores omnis recusandae accusantium nulla cupiditate sit exercitationem eos nam reiciendis molestiae, odio ipsa saepe ducimus.</p>
+                            </dd>
+                        </div>
+                    </dl>
+                </div>
+            </div>
+        </div>
+    </div>
+  </>
+  )
+}
