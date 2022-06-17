db2187ee0c92bbd156b64d86bd1f4c084fb646dd
diikshaaaa <diksha.grover@crownstack.com>
2022-06-17 14:07:35 +0530

    stylef error page

--- app/components/error.tsx
+++ app/components/error.tsx
@@ -14,7 +14,7 @@ export default function ErrorHandler({ name, status }: customError) {
   return (
     <>
       <Links />
-      <div className='min-h-full pt-16 pb-12 flex flex-col bg-white'>
+      <div className='min-h-full pt-16 pb-12 flex flex-col bg-white mb-72 mt-28'>
         <main className='flex-grow flex flex-col justify-center max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8'>
           <div className='flex-shrink-0 flex justify-center'>
             <Link to='/' className='inline-flex'>
