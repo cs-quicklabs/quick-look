f4a0d282d4ca7313c6cb049e5e4bba4ea83f81de
kunalvemra0412 <kverma829@gmail.com>
2022-06-25 14:39:36 +0530

    test repo to test deployment issues

--- app/components/Container.tsx
+++ app/components/Container.tsx
@@ -1,9 +1,10 @@
 import clsx from 'clsx'
 
-export function Container({ className }: any) {
+export function Container({ className, ...props }: any) {
   return (
     <div
       className={clsx('mx-auto max-w-7xl px-4 sm:px-6 lg:px-8', className)}
+      {...props}
     ></div>
   )
 }
