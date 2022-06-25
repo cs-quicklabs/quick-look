70dd37ebd260300cbd163a22f326a1fec155149c
kunalvemra0412 <kverma829@gmail.com>
2022-06-25 15:18:39 +0530

    add

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,25 +1,9 @@
-// import { CallToAction } from '../components/CallToAction'
-// import { Faqs } from '../components/Faqs'
-// import { Footer } from '../components/Footer'
-// import { Hero } from '../components/Hero'
-// import { Pricing } from '../components/Pricing'
-// import { PrimaryFeatures } from '../components/PrimaryFeatures'
-// import { SecondaryFeatures } from '../components/SecondaryFeatures'
-// import { Testimonials } from '../components/Testimonials'
 import Header from '../components/Header'
 const Home = () => {
   return (
-    <>
+    <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
       <Header />
-      {/* <Hero />
-      <PrimaryFeatures />
-      <SecondaryFeatures />
-      <CallToAction />
-      <Testimonials />
-      <Pricing />
-      <Faqs />
-      <Footer /> */}
-    </>
+    </div>
   )
 }
 export default Home
