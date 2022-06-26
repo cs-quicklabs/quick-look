ed01f99a20784023c94295f8efc95e9b3ee0f43d
kunalvemra0412 <kverma829@gmail.com>
2022-06-26 14:29:39 +0530

    Testing deployment issue

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,112 +1,112 @@
-import { Fragment } from 'react'
-import { Popover, Transition } from '@headlessui/react'
+// import { Fragment } from 'react'
+// import { Popover, Transition } from '@headlessui/react'
 
 // import { Container } from '../components/Container'
 
-import clsx from 'clsx'
+// import clsx from 'clsx'
 
-type MobProps = {
-  open: any
-  close: any
-}
+// type MobProps = {
+//   open: any
+//   close: any
+// }
 
-function MobileNavigation() {
-  return (
-    <Popover>
-      {({ open, close }: MobProps) => (
-        <>
-          <Popover.Button className='relative z-10 flex h-8 w-8 items-center justify-center [&:not(:focus-visible)]:focus:outline-none'>
-            <span className='sr-only'>Toggle Navigation</span>
-            <svg
-              aria-hidden='true'
-              className='h-3.5 w-3.5 overflow-visible stroke-slate-700'
-              fill='none'
-              strokeWidth={2}
-              strokeLinecap='round'
-            >
-              <path
-                d='M0 1H14M0 7H14M0 13H14'
-                className={clsx('origin-center transition', {
-                  'scale-90 opacity-0': open,
-                })}
-              />
-              <path
-                d='M2 2L12 12M12 2L2 12'
-                className={clsx('origin-center transition', {
-                  'scale-90 opacity-0': !open,
-                })}
-              />
-            </svg>
-          </Popover.Button>
-          <Transition.Root>
-            <Transition.Child
-              as={Fragment}
-              enter='duration-150 ease-out'
-              enterFrom='opacity-0'
-              enterTo='opacity-100'
-              leave='duration-150 ease-in'
-              leaveFrom='opacity-100'
-              leaveTo='opacity-0'
-            >
-              <Popover.Overlay className='fixed inset-0 bg-slate-300/50' />
-            </Transition.Child>
-            <Transition.Child
-              as={Fragment}
-              enter='duration-150 ease-out'
-              enterFrom='opacity-0 scale-95'
-              enterTo='opacity-100 scale-100'
-              leave='duration-100 ease-in'
-              leaveFrom='opacity-100 scale-100'
-              leaveTo='opacity-0 scale-95'
-            >
-              <Popover.Panel
-                as='ul'
-                className='absolute inset-x-0 top-full mt-4 origin-top space-y-4 rounded-2xl bg-white p-6 text-lg tracking-tight text-slate-900 shadow-xl ring-1 ring-slate-900/5'
-              >
-                <li>
-                  {/* <Link to='#features'> */}
-                  <a
-                    href='#features'
-                    className='block w-full'
-                    onClick={() => close()}
-                  >
-                    Features
-                  </a>
-                  {/* </Link> */}
-                </li>
-                <li>
-                  {/* <Link to='#testimonials'> */}
-                  <a
-                    href='#testimonials'
-                    className='block w-full'
-                    onClick={() => close()}
-                  >
-                    Testimonials
-                  </a>
-                  {/* </Link> */}
-                </li>
-                <li>
-                  {/* <Link to='#pricing'> */}
-                  <a
-                    href='#pricing'
-                    className='block w-full'
-                    onClick={() => close()}
-                  >
-                    Pricing
-                  </a>
-                  {/* </Link> */}
-                </li>
-                <li className='border-t border-slate-300/40 pt-4  font-medium'>
-                  <a href='/login'>Login </a>
-                </li>
-              </Popover.Panel>
-            </Transition.Child>
-          </Transition.Root>
-        </>
-      )}
-    </Popover>
-  )
-}
+// function MobileNavigation() {
+//   return (
+//     <Popover>
+//       {({ open, close }: MobProps) => (
+//         <>
+//           <Popover.Button className='relative z-10 flex h-8 w-8 items-center justify-center [&:not(:focus-visible)]:focus:outline-none'>
+//             <span className='sr-only'>Toggle Navigation</span>
+//             <svg
+//               aria-hidden='true'
+//               className='h-3.5 w-3.5 overflow-visible stroke-slate-700'
+//               fill='none'
+//               strokeWidth={2}
+//               strokeLinecap='round'
+//             >
+//               <path
+//                 d='M0 1H14M0 7H14M0 13H14'
+//                 className={clsx('origin-center transition', {
+//                   'scale-90 opacity-0': open,
+//                 })}
+//               />
+//               <path
+//                 d='M2 2L12 12M12 2L2 12'
+//                 className={clsx('origin-center transition', {
+//                   'scale-90 opacity-0': !open,
+//                 })}
+//               />
+//             </svg>
+//           </Popover.Button>
+//           <Transition.Root>
+//             <Transition.Child
+//               as={Fragment}
+//               enter='duration-150 ease-out'
+//               enterFrom='opacity-0'
+//               enterTo='opacity-100'
+//               leave='duration-150 ease-in'
+//               leaveFrom='opacity-100'
+//               leaveTo='opacity-0'
+//             >
+//               <Popover.Overlay className='fixed inset-0 bg-slate-300/50' />
+//             </Transition.Child>
+//             <Transition.Child
+//               as={Fragment}
+//               enter='duration-150 ease-out'
+//               enterFrom='opacity-0 scale-95'
+//               enterTo='opacity-100 scale-100'
+//               leave='duration-100 ease-in'
+//               leaveFrom='opacity-100 scale-100'
+//               leaveTo='opacity-0 scale-95'
+//             >
+//               <Popover.Panel
+//                 as='ul'
+//                 className='absolute inset-x-0 top-full mt-4 origin-top space-y-4 rounded-2xl bg-white p-6 text-lg tracking-tight text-slate-900 shadow-xl ring-1 ring-slate-900/5'
+//               >
+//                 <li>
+//                   {/* <Link to='#features'> */}
+//                   <a
+//                     href='#features'
+//                     className='block w-full'
+//                     onClick={() => close()}
+//                   >
+//                     Features
+//                   </a>
+//                   {/* </Link> */}
+//                 </li>
+//                 <li>
+//                   {/* <Link to='#testimonials'> */}
+//                   <a
+//                     href='#testimonials'
+//                     className='block w-full'
+//                     onClick={() => close()}
+//                   >
+//                     Testimonials
+//                   </a>
+//                   {/* </Link> */}
+//                 </li>
+//                 <li>
+//                   {/* <Link to='#pricing'> */}
+//                   <a
+//                     href='#pricing'
+//                     className='block w-full'
+//                     onClick={() => close()}
+//                   >
+//                     Pricing
+//                   </a>
+//                   {/* </Link> */}
+//                 </li>
+//                 <li className='border-t border-slate-300/40 pt-4  font-medium'>
+//                   <a href='/login'>Login </a>
+//                 </li>
+//               </Popover.Panel>
+//             </Transition.Child>
+//           </Transition.Root>
+//         </>
+//       )}
+//     </Popover>
+//   )
+// }
 
 export function Header() {
   return (
@@ -197,7 +197,7 @@ export function Header() {
               </a>
             </li>
             <li className='ml-5 -mr-1 md:hidden'>
-              <MobileNavigation />
+              {/* <MobileNavigation /> */}
             </li>
           </ul>
         </nav>
