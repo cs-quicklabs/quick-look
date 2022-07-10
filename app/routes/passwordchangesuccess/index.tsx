f47b1e2a2628abd28c7c43b82f6bba8204938b04
kunalvemra0412 <kverma829@gmail.com>
2022-07-10 16:35:44 +0530

    validation ui fixes

--- app/routes/passwordchangesuccess/index.tsx
+++ app/routes/passwordchangesuccess/index.tsx
@@ -158,7 +158,7 @@ export default function passwordchangesuccess() {
                     })
                   }}
                   className={`appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ${
-                    actionData?.errors['email'] ? 'border border-red-400' : ''
+                    actionData?.errors['password'] ? 'border border-red-400' : ''
                   }`}
                 />
               </div>
