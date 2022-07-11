02ddb8daa8b494ace36f0384f699757300411aa5
kunalvemra0412 <kverma829@gmail.com>
2022-07-11 15:32:54 +0530

    fixed signup form

--- app/routes/Tokenerror/index.tsx
+++ app/routes/Tokenerror/index.tsx
@@ -5,8 +5,7 @@ import logo from '../../../assets/images/logos/quicklook-icon.svg'
 
  return (
     <>
-      
-      <div className='min-h-full pt-16 pb-12 flex flex-col bg-white mb-72 mt-28'>
+      <div className='h-screen pt-16 pb-12 flex flex-col bg-white'>
         <main className='flex-grow flex flex-col justify-center max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8'>
           <div className='flex-shrink-0 flex justify-center'>
             <Link to='/' className='inline-flex'>
