2b44ab9fb4110952b16627dd060493f4f22c6a5d
kunalvemra0412 <kverma829@gmail.com>
2022-07-12 16:25:42 +0530

    fixed dashboard navbar

--- app/components/Common/ConfirmModal.tsx
+++ app/components/Common/ConfirmModal.tsx
@@ -117,7 +117,7 @@ export default function Modal({ open, children, onClose }: any) {
                     className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600 sm:ml-3 sm:w-auto sm:text-sm"
                     onClick={ open}
                   >
-                    Signout
+                    Sign out
                   </button></form>
                   <button
                     type="button"
--- app/routes/dashboard/index.tsx
+++ app/routes/dashboard/index.tsx
@@ -1,54 +1,11 @@
-import { LoaderFunction } from '@remix-run/node'
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
-import Modal from '~/components/Common/ConfirmModal'
-import { requireUserId } from '~/services/auth.service.server'
-import logo from '../../../assets/images/logos/quicklook-icon.svg'
-
-
-export const loader: LoaderFunction = async ({ request }) => {
-  await requireUserId(request)
-  return null
-}
+import HeaderSecondary from "~/components/Common/Header";
 
 export default function Dashboard() {
-  const [isOpen, setIsOpen] = useState(false)
+  // const [isOpen, setIsOpen] = useState(false)
 
   return (
     <>
-      <header className='relative'>
-        <div className='bg-gray-800 pt-2 pb-2'>
-          <nav
-            className='relative mx-auto flex max-w-7xl items-center justify-between px-4 sm:px-6'
-            aria-label='Global'
-          >
-            <div className='flex flex-1 items-center'>
-              <div className='flex w-full items-center justify-between md:w-auto'>
-                <Link
-                  to='/'
-                  className='flex items-center justify-center space-x-2'
-                >
-                  <img className='h-8 w-auto sm:h-10' src={logo} alt='' />
-                  <span className='text-2xl font-bold text-white'>
-                    QuickLook.me
-                  </span>
-                </Link>
-              </div>
-            </div>
-            <div className='hidden md:flex md:items-center md:space-x-2'>
-              <button
-                type='submit'
-                onClick={() => setIsOpen(true)}
-                className='ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-semibold text-white bg-indigo-600 hover:bg-indigo-700 text-center'
-              >
-                Sign out
-              </button>
-
-              <Modal open={isOpen} onClose={() => setIsOpen(false)}></Modal>
-            </div>
-          </nav>
-        </div>
-      </header>
+<HeaderSecondary/>
     </>
   )
 }
