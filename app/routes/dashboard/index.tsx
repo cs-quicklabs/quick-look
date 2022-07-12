789fc1e97eeb759476b594bdf7cbd929ecdc53dd
kunalvemra0412 <kverma829@gmail.com>
2022-07-12 16:54:53 +0530

    auth routes for dashbaord

--- app/routes/dashboard/index.tsx
+++ app/routes/dashboard/index.tsx
@@ -1,7 +1,14 @@
+import { LoaderFunction } from "@remix-run/node";
 import HeaderSecondary from "~/components/Common/Header";
+import { requireUserId } from "~/services/auth.service.server";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  await requireUserId(request)
+  return null
+}
+
 
 export default function Dashboard() {
-  // const [isOpen, setIsOpen] = useState(false)
 
   return (
     <>
