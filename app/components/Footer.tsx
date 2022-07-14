4b55637b8b79555ca1c09e79f10f9e4f996c7a1c
kunalvemra0412 <kverma829@gmail.com>
2022-07-14 15:34:14 +0530

    added mobile responsiveness in footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -26,7 +26,7 @@ export function Footer() {
           </nav>
         </div>
         <div className="flex flex-col items-center border-t border-slate-400/10 py-10 sm:flex-row-reverse sm:justify-between">
-          <div className='flex space-x-6 text-sm'>
+          <div className='flex md:space-x-6 space-x-0 text-sm '>
             <a
                   href='/terms'
                   className='rounded-lg px-2 py-1 text-slate-700 hover:bg-slate-100 hover:text-slate-900'
