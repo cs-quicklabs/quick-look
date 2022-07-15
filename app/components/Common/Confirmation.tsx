8ceffd8045aaa8e77bf9c993d5b8f7d38657feee
Tushar Roy <tushar.roy@crownstack.com>
2022-07-15 15:42:19 +0530

    fix: confirm screens to come under the same route

--- app/components/Common/Confirmation.tsx
+++ app/components/Common/Confirmation.tsx
@@ -9,13 +9,13 @@ export default function Confirmation() {
       <div className='flex flex-col items-center justify-center '>
         <div className='flex flex-col '>
           <img src={logo} alt='' className='h-20 w-20 mt-60 mx-auto' />
-          <p className='text-3xl leading-9 font-extrabold text-center mt-6 text-gray-900'>{Location.pathname.includes('/confirmemail')? 'Confirm your email' : Location.pathname.includes('/confirmforgotpassword')  ? 'Reset Your Password' :''}</p>
+          <p className='text-3xl leading-9 font-extrabold text-center mt-6 text-gray-900'>{Location.pathname.includes('/confirm/email')? 'Confirm your email' : Location.pathname.includes('/confirm/password')  ? 'Reset Your Password' :''}</p>
         </div>
         <div className='flex flex-col w-2/4 max-w-3xl h-auto shadow mt-7 gap-3 pl-6 pb-6 bg-white rounded-lg'>
           <div className='max-w-xl px-4'>
             <h1 className='text-lg leading-6 font-medium mt-6'>Check your Inbox</h1>
             <p className='mt-2 text-sm leading-5 font-normal text-gray-500'>
-             {Location.pathname.includes('/confirmemail')? 'An confirmation email has been sent to you on your specified email Id.Please check your email and click on email confirmation link. You should be able to login into your account after confirming your email.': Location.pathname.includes('/confirmforgotpassword')  ? 'A password reset email has been sent to you on your specified email ID if it exists. Please check your email and click on reset password link. You should be able to login into your account after resetting your password.' :''}
+             {Location.pathname.includes('/confirm/email')? 'An confirmation email has been sent to you on your specified email Id.Please check your email and click on email confirmation link. You should be able to login into your account after confirming your email.': Location.pathname.includes('/confirm/password')  ? 'A password reset email has been sent to you on your specified email ID if it exists. Please check your email and click on reset password link. You should be able to login into your account after resetting your password.' :''}
              
             </p>
             <div className='mt-3'>
