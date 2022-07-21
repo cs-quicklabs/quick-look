376195dbe914b6896c4891e8e9103f9a4e893e41
Tushar Roy <tushar.roy@crownstack.com>
2022-07-21 18:11:11 +0530

    fix: cross

--- app/routes/account/profile.tsx
+++ app/routes/account/profile.tsx
@@ -116,7 +116,6 @@ export const loader: LoaderFunction = async ({ request }) => {
 export default function Profile() {
   const actionData = useActionData()
   const loaderData = useLoaderData()
-console.log(loaderData);
 
   const [val, setVal] = useState({
     firstName: `${loaderData.user.firstname}`,
@@ -124,6 +123,8 @@ console.log(loaderData);
     profileId: `${loaderData.user.username}`,
   })
 
+  const [profileMessage, setProfileMessage] = useState(loaderData.updateProfileMessage)
+  const [passwordMessasge, setPasswordMessage] = useState(loaderData.updatePasswordMessage)
   return (
     <>
       <div>
@@ -134,14 +135,14 @@ console.log(loaderData);
           <ProfileSetting />
         </div>
         <div className="space-y-6 sm:px-6 lg:px-0 lg:col-span-9 ml-56 mt-2 font-inter max-w-xl bg-white">
-          {loaderData.updateProfileMessage ?
+          {profileMessage ?
           <div className="rounded-md bg-green-50 p-4 w-[32.5rem] ml-[1.5rem]">
       <div className="flex">
         <div className="flex-shrink-0">
           <CheckCircleIcon className="h-5 w-5 text-green-400" aria-hidden="true" />
         </div>
         <div className="ml-3">
-          <p className="text-sm font-medium text-green-800">{loaderData.updateProfileMessage}</p>
+          <p className="text-sm font-medium text-green-800">{profileMessage}</p>
         </div>
         <div className="ml-auto pl-3">
           <div className="-mx-1.5 -my-1.5">
@@ -150,7 +151,7 @@ console.log(loaderData);
               className="inline-flex bg-green-50 rounded-md p-1.5 text-green-500 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-green-50 focus:ring-green-600"
             >
               <span className="sr-only">Dismiss</span>
-              <XIcon className="h-5 w-5" aria-hidden="true" onClick={()=>{loaderData.message = null}}/>
+              <XIcon className="h-5 w-5" aria-hidden="true" onClick={()=>{setProfileMessage('')}}/>
             </button>
           </div>
         </div>
@@ -259,14 +260,14 @@ console.log(loaderData);
           <div className='mr-9 ml-5 border-t border-gray-200'>
           </div>
           <div className=''>
-            {loaderData.updatePasswordMessage ?
+            {passwordMessasge ?
           <div className="rounded-md bg-green-50 p-4 w-[32.5rem] ml-[1.5rem]">
       <div className="flex">
         <div className="flex-shrink-0">
           <CheckCircleIcon className="h-5 w-5 text-green-400" aria-hidden="true" />
         </div>
         <div className="ml-3">
-          <p className="text-sm font-medium text-green-800">{loaderData.updatePasswordMessage}</p>
+          <p className="text-sm font-medium text-green-800">{passwordMessasge}</p>
         </div>
         <div className="ml-auto pl-3">
           <div className="-mx-1.5 -my-1.5">
@@ -275,7 +276,7 @@ console.log(loaderData);
               className="inline-flex bg-green-50 rounded-md p-1.5 text-green-500 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-green-50 focus:ring-green-600"
             >
               <span className="sr-only">Dismiss</span>
-              <XIcon className="h-5 w-5" aria-hidden="true" onClick={()=>{loaderData.updatePasswordMessage = null}}/>
+              <XIcon className="h-5 w-5" aria-hidden="true" onClick={()=>{setPasswordMessage('')}}/>
             </button>
           </div>
         </div>
