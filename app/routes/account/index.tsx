85755b7a785697323dab76405cb349cd1a36f22f
Tushar Roy <tushar.roy@crownstack.com>
2022-07-23 11:59:36 +0530

    Account Section

--- app/routes/account/index.tsx
+++ app/routes/account/index.tsx
@@ -7,8 +7,6 @@ import Template1 from "~/components/Templates/template1";
 import { useState } from "react";
 import Template2 from "~/components/Templates/template2";
 
-
-
 export const loader: LoaderFunction = async ({ request }) => {
   await requireUserId(request);
   const user = await getUser(request)
@@ -16,19 +14,16 @@ export const loader: LoaderFunction = async ({ request }) => {
 }
 
 export default function Profile() {
-
   const loaderData = useLoaderData();
   const [show, setshow] = useState(loaderData.templateNumber)
   const [input, setinput] = useState({description:loaderData.bio ,location:loaderData.location,occupation:loaderData.occupation,company:loaderData.company,education:loaderData.education})
-
-
   return (
     <>
       <DashboardHeader username={loaderData.username} />
       <div className='flex'>
-      <AccountSidebar loaderData={loaderData}  setshow={setshow} input={input} setinput={setinput}/>
-      {loaderData.templateNumber === 0 ?
-      <Template1  input={input}  loaderData={loaderData}/> : loaderData.templateNumber === 1 ? <Template2/> : null }
+      <AccountSidebar loaderData={loaderData}  setshow={setshow} input={input} setinput={setinput} />
+      { loaderData.templateNumber == '0' ?
+      <Template1  input={input}  loaderData = {loaderData}/> : loaderData.templateNumber == '1' ? <Template2/> : null }
         </div>
 </>
   
