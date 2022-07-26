ac7b3492aa8af1989426f6dbcaa8d9944805e3aa
kunalvemra0412 <kverma829@gmail.com>
2022-07-26 17:34:41 +0530

    adds ui changes to publish modal

--- app/components/Common/unpublishModal.tsx
+++ app/components/Common/unpublishModal.tsx
@@ -34,8 +34,8 @@ export default function Delete({open,onClose,isPublished}:any) {
             >
               <Dialog.Panel className="relative bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full sm:p-6">
                 <div className="sm:flex sm:items-start">
-                  <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                    <ExclamationIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
+                  <div className={`mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full ${isPublished ? 'bg-red-100' :'bg-indigo-100'} sm:mx-0 sm:h-10 sm:w-10`}>
+                    <ExclamationIcon className={`h-6 w-6 ${isPublished ? 'text-red-600' :'text-indigo-600'} `} aria-hidden="true" />
                   </div>
                   <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                     <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
@@ -53,7 +53,7 @@ export default function Delete({open,onClose,isPublished}:any) {
                  <form action="/account/settings/unpublishAccount">
                   <button
                     type="submit"
-                    className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none sm:w-auto sm:text-sm"
+                    className={`inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 ${isPublished ? 'bg-red-600 hover:bg-red-700' :'bg-indigo-600 hover:bg-indigo-700'}  text-base font-medium text-white  focus:outline-none sm:w-auto sm:text-sm`}
                   >
                     {isPublished ? 'Unpublish' : 'Publish'}
                   </button></form>
