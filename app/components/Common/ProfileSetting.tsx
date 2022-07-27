ceeb804c5a4f65e4b7a6d71dc677bf1107107d1c
Tushar Roy <83631039+TushaRoy@users.noreply.github.com>
2022-07-27 17:07:44 +0530

    fixes ui issues (#158)
    Co-authored-by: kunalvemra0412 <kverma829@gmail.com>

--- app/components/Common/ProfileSetting.tsx
+++ app/components/Common/ProfileSetting.tsx
@@ -131,7 +131,7 @@ export default function ProfileSetting() {
 
     {/* static sidebar */}
       <div className='hidden md:flex md:w-64 md:flex-col md:inset-y-0'>
-          <div className="w-64 flex flex-col flex-grow border-r font-inter border-white mt-8 bg-white overflow-y-auto ml-10">
+          <div className="md:w-[11rem] lg:w-64 flex flex-col flex-grow border-r font-inter border-white mt-8 bg-white overflow-y-auto ml-0 lg:ml-10">
             <div className="flex items-center flex-shrink-0 px-4">
               <h2 className="text-lg leading-6 font-medium text-gray-900">Account</h2>
             </div>
