c3ffbb1461c94fb45db2933669314dd0c7edcece
diikshaaaa <diksha.grover@crownstack.com>
2022-07-29 14:37:06 +0530

    Social Links

--- app/components/SocialProfile/CreateProfile.tsx
+++ app/components/SocialProfile/CreateProfile.tsx
@@ -1,14 +1,106 @@
 import { Fragment, useCallback, useState } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import { XIcon } from '@heroicons/react/outline'
+import SelectSocialProfile from './SelectSocialProfile'
+import ExistingSocialLinks from './ExistingSocialLinks'
 
-export default function CreateProfile({setshowCreateProfile}:any) {
-  const Onclose = (e:any) => {
-   setshowCreateProfile(false)
-  };
+export default function CreateProfile({setshowCreateProfile, setshowSocialLinks}:any) {
+  
   return (
-    <p>
-      Hello
-    </p>
+    <Transition.Root show={true} as={Fragment}>
+      <Dialog as="div" className="relative z-20" onClose={setshowCreateProfile}>
+        <div className="fixed inset-0" />
+
+        <div className="fixed inset-0 overflow-hidden">
+          <div className="absolute inset-0 overflow-hidden">
+            <div className="pointer-events-none fixed inset-y-0 left-0 flex w-96 mt-12">
+              <Transition.Child
+                as={Fragment}
+                enter=""
+                enterFrom="translate-x-full"
+                enterTo="translate-x-0"
+                leave=""
+                leaveFrom="translate-x-0"
+                leaveTo="translate-x-full"
+              >
+                <Dialog.Panel className="pointer-events-auto w-screen max-w-md">
+                  <div className="flex h-full flex-col bg-white border-r w-full md:max-w-xs lg:max-w-md border-gray-200 overflow-y-auto">
+                    <div className="bg-gray-50 py-6 px-4">
+                      <div className="flex items-center justify-between">
+                        <Dialog.Title className="text-lg font-medium leading-7 text-gray-900">
+                          Add Social Profile Links
+                        </Dialog.Title>
+                        <div className="ml-3 flex h-7 items-center">
+                          <button
+                            type="button"
+                            className="rounded-md bg-white text-gray-400 hover:text-gray-600 focus:outline-none focus:ring-2 focus:ring-white"
+                            onClick={() => setshowCreateProfile(false)}
+                          >
+                            <span className="sr-only">Close panel</span>
+                            <XIcon onClick={() => setshowCreateProfile(false)} className="h-6 w-6" aria-hidden="true" />
+                          </button>
+                        </div>
+                      </div>
+
+                      <div className="pt-1 pr-2">
+                        <p className="text-sm leading-5 font-normal text-gray-500">
+                        Select social profile links which you want to share on your profile
+                        </p>
+                      </div>
+                    </div>
+                    <div className='pl-2.5 pr-5 mt-6'>
+                      <div>
+                        <SelectSocialProfile />
+                      </div>
+                      <div className='mt-5'>
+                        <label htmlFor="project-name" className="block text-sm font-medium text-gray-700">
+                          {' '}
+                          Add Link{' '}
+                        </label>
+                        <div className="mt-1">
+                          <input
+                            type="text"
+                            name="location"
+                            id="project-name"
+                            className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm text-gray-500"
+                          />
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="flex flex-shrink-0 justify-end px-4 pb-2 mt-7">
+                      <a href="/account">
+                        <button
+                          type="button"
+                          className="rounded-md mb-4 border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 leading-5"
+                          onClick={() => {
+                            setshowCreateProfile(false);
+                            setshowSocialLinks(false);
+                          }}
+                        >
+                          
+                          Cancel
+                        </button>
+                      </a>
+                      <button
+                        type="submit"
+                        className="ml-4 mr-2 mb-4 leading-5 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700"
+                      >
+                        Add Profile
+                      </button>
+                    </div>
+
+                    <div>
+                      <ExistingSocialLinks />
+                    </div>
+
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
   )
 }
--- app/components/SocialProfile/EmptyProfile.tsx
+++ app/components/SocialProfile/EmptyProfile.tsx
@@ -1,11 +1,8 @@
 import { Fragment, useCallback, useState } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import { XIcon } from '@heroicons/react/outline'
-import thumbnail1 from '../../../assets/images/screenshots/thumbnail1.png'
-import thumbnail2 from '../../../assets/images/screenshots/thumbnail2.png'
 import CreateProfile from './CreateProfile'
-
-
+import ExistingSocialLinks from './ExistingSocialLinks';
 
 export default function EmptyProfile({setshowSocialLinks}:any) {
   const [showCreateProfile, setshowCreateProfile] = useState(false);
@@ -14,9 +11,6 @@ export default function EmptyProfile({setshowSocialLinks}:any) {
     setshowCreateProfile(!showCreateProfile);
   };
 
-  const Onclose = useCallback(() => {
-   setshowSocialLinks(false)
-  }, []);
   return (
     <Transition.Root show={true} as={Fragment}>
       <Dialog as="div" className="relative z-20" onClose={setshowSocialLinks}>
@@ -24,7 +18,7 @@ export default function EmptyProfile({setshowSocialLinks}:any) {
 
         <div className="fixed inset-0 overflow-hidden">
           <div className="absolute inset-0 overflow-hidden">
-            <div className="pointer-events-none fixed inset-y-0 left-0 flex max-w-full mt-12">
+            <div className="pointer-events-none fixed inset-y-0 left-0 flex w-96 mt-12">
               <Transition.Child
                 as={Fragment}
                 enter=""
@@ -34,9 +28,9 @@ export default function EmptyProfile({setshowSocialLinks}:any) {
                 leaveFrom="translate-x-0"
                 leaveTo="translate-x-full"
               >
-                <Dialog.Panel className="pointer-events-auto w-full md:max-w-xs lg:max-w-md">
-                  <div className="flex h-full flex-col bg-white shadow-xl">
-                    <div className="bg-gray-50 py-6 px-4 sm:px-6">
+                <Dialog.Panel className="pointer-events-auto w-screen max-w-md">
+                  <div className="flex h-full flex-col bg-white border-r w-full md:max-w-xs lg:max-w-md border-gray-200 overflow-y-auto">
+                    <div className="bg-gray-50 py-6 px-4">
                       <div className="flex items-center justify-between">
                         <Dialog.Title className="text-lg font-medium leading-7 text-gray-900">Add Social Profile Links</Dialog.Title>
                         <div className="ml-3 flex h-7 items-center">
@@ -56,25 +50,28 @@ export default function EmptyProfile({setshowSocialLinks}:any) {
                         </p>
                       </div>
                     </div>
-                    <div className='font-inter mt-7 pl-12 md:pl-0 lg:pl-14'>
-                      <p className='text-xs leading-4 font-semibold tracking-wide pl-20 md:pl-[6rem]'>
+                    <div className='font-inter mt-7 flex flex-col items-center'>
+                      <p className='text-xs leading-4 font-semibold tracking-wide'>
                         NO LINKS ADDED YET 
                       </p>
-                      <p className='text-sm leading-5 font-normal text-gray-500 md:pl-4 lg:pl-0'>
+                      <p className='text-sm leading-5 font-normal text-gray-500 px-12 lg:px-0'>
                         Please add social links by clicking on button below
                       </p>
                       <button
                         onClick={() => toggleSetting()}
                         type="button"
-                        className="inline-flex items-center px-4 py-2 ml-20 md:ml-[6rem] mt-4 border border-transparent text-sm font-medium leading-5 rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700"
+                        className="inline-flex items-center px-4 py-2 mt-4 border border-transparent text-sm font-medium leading-5 rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700"
                       >
                         Add Social Profile
                       </button>
                       {showCreateProfile && (
-                        <CreateProfile setShowUserSetting={setshowCreateProfile} />
+                        <CreateProfile setshowCreateProfile={setshowCreateProfile} setshowSocialLinks={setshowSocialLinks} />
                       )}
                     </div>
-                  </div>
+                        <div className='mt-12'>
+                          <ExistingSocialLinks />
+                        </div>
+                  </div>                  
                 </Dialog.Panel>
               </Transition.Child>
             </div>
