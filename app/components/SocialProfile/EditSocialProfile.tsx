627c45553068d4eb6ac612c7b5eff1847ef0fc12
diikshaaaa <diksha.grover@crownstack.com>
2022-08-02 12:40:07 +0530

    cancel button fixed

--- app/components/SocialProfile/EditSocialProfile.tsx
+++ app/components/SocialProfile/EditSocialProfile.tsx
@@ -90,18 +90,18 @@ export default function EditSocialProfile({loaderData, setShowEditProfile, setsh
                     </div>
 
                     <div className="flex flex-shrink-0 justify-end px-4 pb-2 mt-7">
-                      <a href="/account">
+                      <span>
                         <button
                           type="button"
                           className="rounded-md mb-4 border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 leading-5"
                           onClick={() => {
                             setShowEditProfile(false);
-                            setshowSocialLinks(false);
+                            setshowSocialLinks(true);
                           }}
                         >
                           Cancel
                         </button>
-                      </a>
+                      </span>
                       <button
                         type="submit"
                         className="ml-4 mr-2 mb-4 leading-5 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700"
