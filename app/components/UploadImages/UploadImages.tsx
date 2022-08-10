4457a91533dcb606cc205da2ee1ec494ee649697
kunalvemra0412 <kverma829@gmail.com>
2022-08-10 12:57:22 +0530

    added validation for social link,fixed z-index for logout modal

--- app/components/UploadImages/UploadImages.tsx
+++ app/components/UploadImages/UploadImages.tsx
@@ -170,7 +170,7 @@ export default function NoImages({ setshowImages, mode, setmode, setPrimaryResto
                                   {/* <input type="file" name="photo" /> */}
                                   <button type="submit" ref={ref} className="hidden">upload</button>
                                 </label>
-                                <button name='primaryrestore' className="cursor-pointer text-sm leading-5 mt-2.5 font-normal text-gray-400 hover:text-gray-600" onClick={(e) => { ; e.preventDefault() }}>
+                                <button name='primaryrestore' className="cursor-pointer text-sm leading-5 mt-2.5 font-normal text-gray-400 hover:text-gray-600" onClick={(e) => e.preventDefault() }>
                                   Restore Default Image
                                 </button>
                               </div>
