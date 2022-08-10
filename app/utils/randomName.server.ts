213c3519ec060d199e12438ed006786f823cf982
Tushar Roy <tushar.roy@crownstack.com>
2022-08-10 12:26:21 +0530

    add: utils for generating random name

--- app/utils/randomName.server.ts
+++ app/utils/randomName.server.ts
@@ -1,4 +1,4 @@
-export const randomName = async() => {
+export const randomNames = async() => {
     Array(32)
     .fill(null)
     .map(() => Math.round(Math.random() * 16).toString(16))
@@ -6,4 +6,13 @@ export const randomName = async() => {
     
     return `uploads/images/${Date.now().toString()}-${randomName}
     )}`;
+}
+
+export const randomName =async (): Promise<string> => {
+    const key = Array(32)
+    .fill(null)
+    .map(() => Math.round(Math.random() * 16).toString(16))
+    .join('');
+
+    return key
 }
