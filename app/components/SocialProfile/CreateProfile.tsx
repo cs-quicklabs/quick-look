69f0042b898275bbbd8dc29646fa7b0910cea428
Tushar Roy <tushar.roy@crownstack.com>
2022-08-16 14:50:10 +0530

    fix: server error

--- app/components/SocialProfile/CreateProfile.tsx
+++ app/components/SocialProfile/CreateProfile.tsx
@@ -184,14 +184,13 @@ console.log('###################', actionData)
                             Cancel
                           </button>
                         </div>
-                        <Link to='/account' reloadDocument>
+                        
                         <button
                           //type="submit"
                           className="ml-4 mr-2 mb-4 leading-5 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700" 
                         >
                           Add Profile
                         </button>
-                        </Link>
                       </div>
 
                       <div className='mt-10'>
