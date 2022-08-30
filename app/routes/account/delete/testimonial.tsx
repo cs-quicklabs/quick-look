7e5f7b67063bbaa1a635538a6b1a751adae5d61c
diikshaaaa <diksha.grover@crownstack.com>
2022-08-30 13:21:26 +0530

    functionality of testimonial is working and ui for add video

--- app/routes/account/delete/testimonial.tsx
+++ app/routes/account/delete/testimonial.tsx
@@ -2,7 +2,6 @@ import { ActionFunction, redirect } from "@remix-run/node";
 import { getUser } from "~/services/auth.service.server";
 import { commitSession, getSession } from "~/services/session.service.server";
 import { deleteTestimonial } from "~/services/testimonial.service.server";
-import { deleteImage } from "~/services/user.service.serevr";
 
 export const action: ActionFunction = async ({request}) => {
     const user = await getUser(request) || undefined
