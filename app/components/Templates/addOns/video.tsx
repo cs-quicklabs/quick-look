db76062155135480a58987a965fd1f59686d7ddf
Tushar Roy <tushar.roy@crownstack.com>
2022-08-31 19:16:30 +0530

    optional chaining fix

--- app/components/Templates/addOns/video.tsx
+++ app/components/Templates/addOns/video.tsx
@@ -10,7 +10,7 @@ export default function VideoAddOn(videoLink: any) {
   return (
     <div className='max-w-7xl mx-auto mt-8 '>
       {/* {videoLink.videoSourceKey === 'youtube' ?  */}
-    {link.includes('youtube') ? 
+    {link?.includes('youtube') ? 
     <iframe className='w-full h-[25rem]' src= {`https://www.youtube.com/embed/${youtubeVideoId}`} /> 
       :
       <iframe src={`https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.${youtubeVideoId}`}  width="734" height="700"  scrolling="no"  allowTransparency={true} allowFullScreen={true} />}
