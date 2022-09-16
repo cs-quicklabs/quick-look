b1365822362767d48ffd44a96d379d912ea9f808
Tushar Roy <tushar.roy@crownstack.com>
2022-09-16 18:19:34 +0530

    add user redirection

---
+++ app/routes/$username/index.tsx
@@ -0,0 +1,28 @@
+import { ActionFunction, json, LoaderFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { useState } from "react";
+import Template1 from "~/components/Templates/template1";
+import Template2 from "~/components/Templates/template2";
+import { getUserByUsername } from "~/services/user.service.serevr";
+
+
+
+export const loader: LoaderFunction = async ({params}) => {
+
+    const user = await getUserByUsername(params?.username!)
+    return json(user)
+
+  }
+
+  export default function ProfileView() {
+
+    const loaderData =  useLoaderData()
+    const primaryRestore = loaderData?.profileImage?.isUsingPrimaryDefault
+    const secondaryRestore = loaderData?.profileImage?.isUsingSecondaryDefault
+    const [input, setinput] = useState({description:loaderData?.profileInfo?.bio ,location:loaderData?.profileInfo?.location,occupation:loaderData?.profileInfo?.occupation,company:loaderData?.profileInfo?.company,education:loaderData?.profileInfo?.education})
+    return (
+        <div >
+        { loaderData?.profileInfo?.templateNumber == '0' ?
+        <Template1 primaryRestore={primaryRestore} secondaryRestore={secondaryRestore} input={input}  loaderData = {loaderData}/> : loaderData?.profileInfo?.templateNumber == '1' ? <Template2 secondaryRestore={secondaryRestore} input={input}  loaderData = {loaderData}/> : null }</div>
+    )
+  }
