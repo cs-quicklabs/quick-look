02ec136af8c2acd52277da3883e4d1a2cad1df1b
Tushar Roy <tushar.roy@crownstack.com>
2022-09-19 21:16:06 +0530

    adds: google recaptcha

--- app/routes/auth/signup/index.tsx
+++ app/routes/auth/signup/index.tsx
@@ -23,7 +23,7 @@ import { useRef } from 'react';
 import axios from 'axios'
 
 export const action: ActionFunction = async ({ request }) => {
-  
+
   const {
     firstname,
     lastname,
@@ -46,7 +46,7 @@ export const action: ActionFunction = async ({ request }) => {
     lastname: await validateLastName(lastname),
     username: await validateUsername(username, false),
     isPasswordSame: await validateComfirmPassword(password, confirmPassword),
-    captchaTokenError: captchaToken ? undefined : 'Incorrect Captcha',  
+    captchaTokenError: captchaToken ? undefined : 'Incorrect Captcha',
     captchaError: res.status == 200 ? undefined : 'Incorrect Captcha'
   }
 
@@ -80,9 +80,9 @@ export const action: ActionFunction = async ({ request }) => {
   return redirect('/confirm/email')
 }
 
-export const loader: LoaderFunction = async ({request}) => {
+export const loader: LoaderFunction = async ({ request }) => {
   const user = await getUser(request)
-  if(user){
+  if (user) {
     return redirect('/account')
   }
   return null
@@ -92,21 +92,15 @@ export default function SignUp() {
   const captchaRef = useRef(null)
   const transition = useTransition()
   const actionData = useActionData()
-  const[token,setToken]=useState('')
-  
-  console.log(captchaRef);
-  
+  const [token, setToken] = useState('')
 
-  const handleSubmit = async (e:any) =>{
-     //@ts-ignore
-        setToken(captchaRef.current.getValue())
-        console.log(token)
-        //@ts-ignore
-        captchaRef.current.execute();
-    }
-    
-
-
+  const handleSubmit = async (e: any) => {
+    //@ts-ignore
+    setToken(captchaRef.current.getValue())
+    console.log(token)
+    //@ts-ignore
+    captchaRef.current.execute();
+  }
 
   const [val, setVal] = useState({
     firstName: '',
@@ -123,14 +117,14 @@ export default function SignUp() {
         <div className='mx-auto flex flex-col items-center justify-center mt-20'>
           <img src={logo} alt='' className='h-20 w-20' />
           <div className='flex flex-col items-center justify-center'>
-          <h2 className='w-full h-9 mt-6 font-[800] text-center text-3xl  leading-9 text-gray-900'>
-            Create new Account
-          </h2>
-          <p className='mt-2 text-center text-sm font-inter font-medium'>
-            
+            <h2 className='w-full h-9 mt-6 font-[800] text-center text-3xl  leading-9 text-gray-900'>
+              Create new Account
+            </h2>
+            <p className='mt-2 text-center text-sm font-inter font-medium'>
+
               No credit card required. Starting with free plan.
-            
-          </p></div>
+
+            </p></div>
         </div>
         <div className='mt-5 mx-auto font-inter'>
           <div className='bg-gray-50 '>
@@ -142,11 +136,10 @@ export default function SignUp() {
                   </label>
                   <input
                     data-cy="firstName"
-                    className={`flex items-center box-border appearance-none w-44 h-10 px-2.5 py-3.5 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm mt-1.5 ${
-                      actionData?.errors['firstname']
+                    className={`flex items-center box-border appearance-none w-44 h-10 px-2.5 py-3.5 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm mt-1.5 ${actionData?.errors['firstname']
                         ? 'border border-red-400'
                         : 'first-line:'
-                    }`}
+                      }`}
                     type='firstName'
                     name='firstName'
                     value={val.firstName}
@@ -156,17 +149,17 @@ export default function SignUp() {
                         [event.target.name]: event.target.value,
                       })
                     }}
-                    
+
                   />
                   {actionData?.errors['firstname'] ?
-                <div className={`absolute  pr-3 right-0 pt-1.5 flex items-center pointer-events-none inset-y-0 `} >
-                  <ExclamationCircleIcon className="h-4 w-4 text-red-500" aria-hidden="true" />
-                </div>:''}
-                  
-{actionData?.errors['firstname'] ?
-                  <div className='text-red-600 text-sm w-44 h-[1.8rem]'>
-                    {actionData?.errors['firstname']}
-                  </div> :null}
+                    <div className={`absolute  pr-3 right-0 pt-1.5 flex items-center pointer-events-none inset-y-0 `} >
+                      <ExclamationCircleIcon className="h-4 w-4 text-red-500" aria-hidden="true" />
+                    </div> : ''}
+
+                  {actionData?.errors['firstname'] ?
+                    <div className='text-red-600 text-sm w-44 h-[1.8rem]'>
+                      {actionData?.errors['firstname']}
+                    </div> : null}
                 </div>
                 <div className='relative'>
                   <label className='text-gray-700 w-24 h-5 font-medium leading-5 text-sm'>
@@ -174,11 +167,10 @@ export default function SignUp() {
                   </label>
                   <input
                     data-cy="lastName"
-                    className={`flex items-center box-border appearance-none w-44 h-10 px-2.5 py-3.5 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm mt-1.5 ${
-                      actionData?.errors['lastname']
+                    className={`flex items-center box-border appearance-none w-44 h-10 px-2.5 py-3.5 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm mt-1.5 ${actionData?.errors['lastname']
                         ? 'border border-red-400'
                         : 'first-line:'
-                    }`}
+                      }`}
                     type='lastName'
                     name='lastName'
                     value={val.lastName}
@@ -190,68 +182,67 @@ export default function SignUp() {
                     }}
                   />
                   {actionData?.errors['lastname'] ?
-                <div className={`absolute  pr-3 right-0 pt-1.5 flex items-center pointer-events-none inset-y-0`} >
-                  <ExclamationCircleIcon className="h-4 w-4 text-red-500" aria-hidden="true" />
-                </div>:''}
+                    <div className={`absolute  pr-3 right-0 pt-1.5 flex items-center pointer-events-none inset-y-0`} >
+                      <ExclamationCircleIcon className="h-4 w-4 text-red-500" aria-hidden="true" />
+                    </div> : ''}
                   {/* <div className='text-red-600 text-sm w-44 h-[2rem]'>
                     {actionData?.errors['lastname']}
                   </div> */}
-                     {actionData?.errors['lastname'] ?
-                  <div className='text-red-600 text-sm w-44 h-[1.8rem]'>
+                  {actionData?.errors['lastname'] ?
+                    <div className='text-red-600 text-sm w-44 h-[1.8rem]'>
                       {actionData?.errors['lastname']}
-                  </div> :null}
+                    </div> : null}
                 </div>
               </div>
 
-            <div>
-              <label className='text-gray-700 w-36 h-5 mt-4 font-medium leading-5 text-sm'>
-                Choose your Profile ID
-              </label>
-              <div className={`mt-2 sm:mt-0 sm:col-span-2 relative rounded-md shadow-sm`}>
-                <div className={`max-w-lg flex rounded-md`}>
-                  <span className={`inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 sm:text-sm ${actionData?.errors['username']
-                        ? 'border-t border-b border-l border-r border-r-gray-300 border-red-400'
+              <div>
+                <label className='text-gray-700 w-36 h-5 mt-4 font-medium leading-5 text-sm'>
+                  Choose your Profile ID
+                </label>
+                <div className={`mt-2 sm:mt-0 sm:col-span-2 relative rounded-md shadow-sm`}>
+                  <div className={`max-w-lg flex rounded-md`}>
+                    <span className={`inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 sm:text-sm ${actionData?.errors['username']
+                      ? 'border-t border-b border-l border-r border-r-gray-300 border-red-400'
+                      : 'first-line:'
+                      }`}>
+                      quicklook.me/
+                    </span>
+                    <input
+                      data-cy="profileId"
+                      type='text'
+                      name='profileId'
+                      value={val.profileId}
+                      onChange={(event) => {
+                        setVal({
+                          ...val,
+                          [event.target.name]: event.target.value,
+                        })
+                      }}
+                      className={`flex-1 block w-full focus:ring-indigo-500 focus:border-indigo-500 min-w-0 rounded-none rounded-r-md sm:text-sm border-gray-300 border  ${actionData?.errors['username']
+                        ? 'border-t border-b border-r border-l-0 border-red-400'
                         : 'first-line:'
-                    }`}>
-                    quicklook.me/
-                  </span>
-                  <input
-                    data-cy="profileId"
-                    type='text'
-                    name='profileId'
-                    value={val.profileId}
-                    onChange={(event) => {
-                      setVal({
-                        ...val,
-                        [event.target.name]: event.target.value,
-                      })
-                    }}
-                    className={`flex-1 block w-full focus:ring-indigo-500 focus:border-indigo-500 min-w-0 rounded-none rounded-r-md sm:text-sm border-gray-300 border  ${actionData?.errors['username']
-                    ? 'border-t border-b border-r border-l-0 border-red-400'
-                    : 'first-line:'
-                }`}
-                  />
+                        }`}
+                    />
+                  </div>
+                  {actionData?.errors['username'] ?
+                    <div className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+                      <ExclamationCircleIcon className="h-4 w-4 text-red-500" aria-hidden="true" />
+                    </div> : ''}
                 </div>
-                {actionData?.errors['username'] ?
-                <div className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
-                  <ExclamationCircleIcon className="h-4 w-4 text-red-500" aria-hidden="true" />
-                </div>:''}
+                <div className='text-red-600 text-sm'>
+                  {actionData?.errors['username']}
+                </div>
               </div>
-              <div className='text-red-600 text-sm'>
-                {actionData?.errors['username']}
-              </div>
-            </div>
 
-            
+
               <div className='relative'>
                 <label className='text-gray-700 w-36 h-5 mt-4 font-medium leading-5 text-sm'>
                   Email address
                 </label>
                 <input
                   data-cy="email"
-                  className={`box-border appearance-none block w-full h-10 px-2.5 py-3.5 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm mt-1.5 ${
-                    actionData?.errors['email'] ? 'border border-red-400' : ''
-                  }`}
+                  className={`box-border appearance-none block w-full h-10 px-2.5 py-3.5 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm mt-1.5 ${actionData?.errors['email'] ? 'border border-red-400' : ''
+                    }`}
                   type='email'
                   name='email'
                   value={val.email}
@@ -263,9 +254,9 @@ export default function SignUp() {
                   }}
                 />
                 {actionData?.errors['email'] ?
-                <div className="absolute inset-y-0 right-0 pr-3 pt-1.5 flex items-center pointer-events-none ">
-                  <ExclamationCircleIcon className="h-4 w-4 text-red-500" aria-hidden="true" />
-                </div>:''}
+                  <div className="absolute inset-y-0 right-0 pr-3 pt-1.5 flex items-center pointer-events-none ">
+                    <ExclamationCircleIcon className="h-4 w-4 text-red-500" aria-hidden="true" />
+                  </div> : ''}
                 <div className='text-red-600 text-sm '>
                   {actionData?.errors['email']}
                 </div>
@@ -276,11 +267,10 @@ export default function SignUp() {
                 </label>
                 <input
                   data-cy="password"
-                  className={`box-border appearance-none block w-full h-10 px-2.5 py-3.5 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm mt-1.5 ${
-                    actionData?.errors['password']
+                  className={`box-border appearance-none block w-full h-10 px-2.5 py-3.5 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm mt-1.5 ${actionData?.errors['password']
                       ? 'border border-red-400'
                       : ''
-                  }`}
+                    }`}
                   type='password'
                   name='password'
                   value={val.password}
@@ -292,9 +282,9 @@ export default function SignUp() {
                   }}
                 />
                 {actionData?.errors['password'] ?
-                <div className="absolute inset-y-0 right-0 pr-3 pt-1.5 flex items-center pointer-events-none ">
-                  <ExclamationCircleIcon className="h-4 w-4 text-red-500" aria-hidden="true" />
-                </div>:''}
+                  <div className="absolute inset-y-0 right-0 pr-3 pt-1.5 flex items-center pointer-events-none ">
+                    <ExclamationCircleIcon className="h-4 w-4 text-red-500" aria-hidden="true" />
+                  </div> : ''}
                 <div className='text-red-600 text-sm '>
                   {actionData?.errors['password']}
                 </div>
@@ -305,11 +295,10 @@ export default function SignUp() {
                 </label>
                 <input
                   data-cy="confirmPassword"
-                  className={`box-border appearance-none block w-full h-10 px-2.5 py-3.5 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm mt-1.5 ${
-                    actionData?.errors['isPasswordSame']
+                  className={`box-border appearance-none block w-full h-10 px-2.5 py-3.5 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm mt-1.5 ${actionData?.errors['isPasswordSame']
                       ? 'border border-red-400'
                       : ''
-                  }`}
+                    }`}
                   type='password'
                   name='confirmPassword'
                   value={val.confirmPassword}
@@ -321,42 +310,42 @@ export default function SignUp() {
                   }}
                 />
                 {actionData?.errors['isPasswordSame'] ?
-                <div className="absolute inset-y-0 right-0 pr-3 pt-1.5 flex items-center pointer-events-none ">
-                  <ExclamationCircleIcon className="h-4 w-4 text-red-500" aria-hidden="true" />
-                </div>:''}
+                  <div className="absolute inset-y-0 right-0 pr-3 pt-1.5 flex items-center pointer-events-none ">
+                    <ExclamationCircleIcon className="h-4 w-4 text-red-500" aria-hidden="true" />
+                  </div> : ''}
                 <div className='text-red-600 text-sm '>
                   {actionData?.errors['isPasswordSame']}
                 </div>
               </div>
               <div className='flex flex-col justify-end'>
-              <ReCAPTCHA ref={captchaRef} sitekey='6LdMrsQhAAAAALgzO0zsjuRPDxFQDFs-alzf35m0'  onChange={handleSubmit}/>
-              <div className='text-red-600 text-sm'>
-                {actionData?.errors['captchaError']} 
-              </div>
-              <div className='text-red-600 text-sm'>
-              {actionData?.errors['captchaTokenError']}
-              </div>
-              <input type="text" hidden name='captcha' value={token}/>
+                <ReCAPTCHA  ref={captchaRef} sitekey='6LdMrsQhAAAAALgzO0zsjuRPDxFQDFs-alzf35m0' onChange={handleSubmit} />
+                <div className='text-red-600 text-sm'>
+                  {actionData?.errors['captchaError']}
+                </div>
+                <div className='text-red-600 text-sm'>
+                  {actionData?.errors['captchaTokenError']}
+                </div>
+                <input type="text" hidden name='captcha' value={token} />
                 <button
                   data-cy="createNewAccountButton"
                   type="submit"
                   className={`group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white leading-5 bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mt-8`}
                   disabled={transition?.state != "idle" ? true : false}
-                  >
-                
+                >
+
                   <span className='absolute left-0 inset-y-0 flex items-center pl-3'>
                     <LockClosedIcon
                       className={`h-5 w-5 text-indigo-500 group-hover:text-indigo-400 `}
                       aria-hidden='true'
                     />
                   </span>
-                  {transition?.state != "idle"  ? <BeatLoader color="#ffffff" /> :
+                  {transition?.state != "idle" ? <BeatLoader color="#ffffff" /> :
                     "Create New Account"}
-                  
+
                 </button>
               </div>
             </Form>
-            
+
           </div>
         </div>
       </div>
