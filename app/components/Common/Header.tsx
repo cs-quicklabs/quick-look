92a9a920fca4b4e150dc99263ee29f0fbb56ad5e
kunalvemra0412 <kverma829@gmail.com>
2022-09-23 19:05:47 +0530

    fixed QM-213

--- app/components/Common/Header.tsx
+++ app/components/Common/Header.tsx
@@ -17,8 +17,8 @@ export default function HeaderSecondary() {
         >
 
           <div className='flex w-full items-center justify-between md:w-auto '>
-            <Link
-              to='/'
+            <a
+              href='/'
               className='flex items-center justify-center gap-4 -mt-[12px]'
             >
               <img className='w-auto h-9 mt-[8px]' src={logo} alt='' />
@@ -26,26 +26,26 @@ export default function HeaderSecondary() {
               <span className='text-xl pt-5 pb-3 font-extrabold text-white'>
                 QuickLook.me
               </span>
-            </Link>
+            </a>
           </div>
 
           <div className='hidden md:flex md:items-center md:space-x-2 mr-6 '>
             {Location.pathname.includes('/auth/signup') ||
               Location.pathname.includes('/forgot-password') ? (
-              <Link
-                to='/auth/login'
+              <a
+                href='/auth/login'
                 className='h-8 mb-1 font-[500] whitespace-nowrap inline-flex items-center justify-center border border-transparent rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 text-center group py-2.5 px-4 text-sm focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2  hover:text-slate-100  active:bg-blue-800 active:text-blue-100 focus-visible:outline-blue-600'
               >
                 Sign in to your Account
-              </Link>
+              </a>
 
             ) : (
-              <Link
-                to='/auth/signup'
+              <a
+                href='/auth/signup'
                 className='h-8 mb-1 whitespace-nowrap inline-flex items-center justify-center border border-transparent rounded shadow-sm font-[500] text-white bg-indigo-600 hover:bg-indigo-700 text-center group py-2.5 px-4 text-sm focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2  hover:text-slate-100  active:bg-blue-800 active:text-blue-100 focus-visible:outline-blue-600'
               >
                 Get Started For Free
-              </Link>
+              </a>
 
             )}
           </div>
