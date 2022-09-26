258bf14e9b333d973d135b86ff05576d110a5ae9
kunalvemra0412 <kverma829@gmail.com>
2022-09-26 16:19:50 +0530

    fixed QM-216

--- app/components/Templates/addOns/Banner.tsx
+++ app/components/Templates/addOns/Banner.tsx
@@ -9,12 +9,12 @@ export default function BannerAddOn({loaderData}:any) {
   const Name = _.replace(iconName, ' ', '');
   const Final = Name.split(" ").join('')
 
-  const bannerText = loaderData?.supportBanner?.bannerText?.length > 30 ? `${loaderData?.supportBanner?.bannerText.substring(0, 30)}...` : loaderData?.supportBanner?.bannerText
+  
   return (
     <div className='flex justify-center items-center'>
       <button style={{ 'backgroundColor': loaderData?.supportBanner?.bannerHex , 'textShadow': '0px 1px black' }} className={`flex justify-center items-center h-10 w-full shadow-sm text-sm leading-5 font-medium text-white cursor-default ${loaderData?.supportBanner?.bannerColor}`}>
         <DynamicHeroIcon icon={Final} Final={Final} />
-        <a href={loaderData?.supportBanner?.bannerlink.includes('https') ? loaderData?.supportBanner?.bannerlink : 'https://'+loaderData?.supportBanner?.bannerlink} target="_blank" className='hover:underline text-sm leading-5 font-semibold'>{bannerText} </a>
+        <a href={loaderData?.supportBanner?.bannerlink.includes('https') ? loaderData?.supportBanner?.bannerlink : 'https://'+loaderData?.supportBanner?.bannerlink} target="_blank" className='hover:underline text-sm leading-5 font-semibold'>{loaderData?.supportBanner?.bannerText} </a>
       </button>
   </div>
   )
