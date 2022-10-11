07d21450d04d85cfaab23dcd633bb19e4f0d67cf
kunalvemra0412 <kverma829@gmail.com>
2022-10-11 19:18:07 +0530

    added gallery

--- app/components/Templates/addOns/portfolio.tsx
+++ app/components/Templates/addOns/portfolio.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 import LightGallery from "lightgallery/react";
 import lgZoom from "lightgallery/plugins/zoom";
 import lgVideo from "lightgallery/plugins/video";
-import '../../../styles/input.css'
+// import '../../../styles/tailwind.css'
+
  export default function PortfolioAddon({loaderData}:any) {
  
   console.log(loaderData)
@@ -11,58 +12,35 @@ const images = loaderData?.portfolioImage.map((img:any) => (
     original: img.imageUrl
   }
 ))
+const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      initialSlide: 2
+    };
   return (
-   <>
+   <div className='img'>
+ 
    <LightGallery plugins={[lgZoom, lgVideo]} mode="lg-fade">
+    {/* <div className='grid grid-cols-4  gap-4 items-center'> */}
+   {loaderData.portfolioImage.map((img:any)=>(
         <a
           data-lg-size="1406-1390"
           className="gallery-item"
-          data-src="https://images.unsplash.com/photo-1581894158358-5ecd2c518883?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1406&q=80"
-          data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@entrycube' >Diego Guzmán </a></h4> <p> Location - <a href='https://unsplash.com/s/photos/fushimi-inari-taisha-shrine-senbontorii%2C-68%E7%95%AA%E5%9C%B0-fukakusa-yabunouchicho%2C-fushimi-ward%2C-kyoto%2C-japan'>Fushimi Ward, Kyoto, Japan</a></p>"
+          data-src={img.imageUrl}
+          
         >
           <img
             className="img-responsive"
-            src="https://images.unsplash.com/photo-1581894158358-5ecd2c518883?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=240&q=80"
+            src={img.imageUrl}
           />
         </a>
-        <a
-          data-lg-size="1400-1400"
-          data-pinterest-text="Shinimamiya, Osaka, Japan"
-          data-tweet-text="Shinimamiya, Osaka, Japan"
-          className="gallery-item"
-          data-src="https://images.unsplash.com/photo-1544550285-f813152fb2fd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80"
-          data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@asoshiation' >Shah </a></h4><p> Location - <a href='https://unsplash.com/s/photos/shinimamiya%2C-osaka%2C-japan'>Shinimamiya, Osaka, Japan</a></p>"
-        >
-          <img
-            className="img-responsive"
-            src="https://images.unsplash.com/photo-1544550285-f813152fb2fd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=240&q=80"
-          />
-        </a>
-        <a
-          data-lg-size="1400-1400"
-          className="gallery-item"
-          data-src="https://images.unsplash.com/photo-1584592740039-cddf0671f3d4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80"
-          data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@katherine_xx11' >Katherine Gu </a></h4><p> For all those years we were alone and helpless.</p>"
-        >
-          <img
-            className="img-responsive"
-            src="https://images.unsplash.com/photo-1584592740039-cddf0671f3d4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=240&q=80"
-          />
-        </a>
-        <a
-          className="gallery-item"
-          data-src="https://www.youtube.com/watch?v=egyIeygdS_E&mute=0"
-          key="4"
-        >
-          <img
-            style={{ maxWidth: "400px" }}
-            className="img-responsive"
-            alt=""
-            src="https://img.youtube.com/vi/egyIeygdS_E/maxresdefault.jpg"
-          />
-        </a>
-      </LightGallery>
 
-    </>
+))}
+{/* </div> */}
+      </LightGallery>
+    </div>
   )
 }
