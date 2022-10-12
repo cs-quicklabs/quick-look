476a0d9cf67a1eb2b03175319aab3464e1132616
diikshaaaa <diksha.grover@crownstack.com>
2022-10-12 11:58:27 +0530

    unpublish account error fixed

--- app/routes/unpublishedAccountError.tsx
+++ app/routes/unpublishedAccountError.tsx
@@ -1,5 +1,5 @@
 import tailwindStylesheetUrl from '../styles/tailwind.css'
-import { Link, Links } from '@remix-run/react'
+import { Links } from '@remix-run/react'
 import logo from '../../assets/images/logos/quicklook-icon.svg'
 
 export function links() {
@@ -10,31 +10,16 @@ export default function PublishAccount() {
   return (
     <>
       <Links />
-      <div className='min-h-full pt-16 pb-12 flex flex-col bg-white mb-72 mt-28'>
-        <main className='flex-grow flex flex-col justify-center max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8'>
+      <div className='flex flex-col bg-white h-screen overflow-hidden flex flex-col justify-center'>
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <img src={logo} alt='' className='ml-48 h-20 w-20' />
-          <div className='flex-shrink-0 flex justify-center'>
-            <Link to='/' className='inline-flex'>
-              <span className='sr-only'>Quicklook</span>
-            </Link>
-          </div>
-          <div className='py-16'>
-            <div className='text-center'>
-              <h1 className='mt-2 text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl'>
-              The account you are looking for is not yet published. Please publish your account so that anyone can visit your profile.
-              </h1>
-              <div className='flex items-center justify-center'>
-              <button
-                  type="submit"
-                  className={`group relative w-[22rem] flex items-center justify-center py-2 px-4 border border-transparent text-xl font-medium rounded-md text-white leading-5 bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mt-8`}
-                >
-                  Publish account
-                </button>
-              </div>
-             
-            </div>
-          </div>
-        </main>
+        </div>
+      <div className='text-center py-2'>
+        <h1 className='mt-2 text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl'>
+          The account you are looking for is not yet published.
+        </h1>
+      </div>
+        
       </div>
     </>
   )
