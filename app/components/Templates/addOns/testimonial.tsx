f277ca88b4fa9c76d33d361d07445cd399f4a4be
kunalvemra0412 <kverma829@gmail.com>
2022-10-26 12:24:18 +0530

    fixed width issue in testimonials in tempalte 10

--- app/components/Templates/addOns/testimonial.tsx
+++ app/components/Templates/addOns/testimonial.tsx
@@ -1,35 +1,38 @@
 import React from 'react'
 
-export default function TestimonialAddOn({testimonialText, testimonialBy}:any) {
+export default function TestimonialAddOn({
+  testimonialText,
+  testimonialBy,
+  loaderData,
+}: any) {
   return (
     <section className="mt-24 overflow-hidden">
-            <div className="max-w-7xl mx-auto">
+      <div
+        className={`mx-auto ${
+          loaderData?.profileInfo?.templateNumber == '10'
+            ? 'w-[32rem]'
+            : 'max-w-7xl '
+        }`}
+      >
+        <div className="">
+          <blockquote className="">
+            <div className="text-base font-normal leading-5 text-gray-500">
+              <div className="mb-[-3rem] flex justify-start text-7xl text-black">
+                &ldquo;
+              </div>
+              <pre className="flex whitespace-pre-wrap pt-2 text-center font-sans text-base font-normal leading-5 text-gray-500 ">
+                {testimonialText}
+              </pre>
+              <br />
+              <div className="flex justify-end">-- {testimonialBy}</div>
 
-              <div className="">
-
-                <blockquote className="">
-                  <div className="text-base leading-5 font-normal text-gray-500">
-                    
-                      <div className='text-7xl flex justify-start mb-[-3rem] text-black'>
-                        &ldquo;
-                      </div> 
-                      <pre className="text-gray-500 text-center pt-2 text-base leading-5 font-normal font-sans flex whitespace-pre-wrap ">
-                        {testimonialText}
-                      </pre>
-                      <br />
-                      <div className='flex justify-end'>
-                      -- {testimonialBy}
-                      </div>
-                    
-                      <div className='text-7xl flex justify-end mb-[-3rem] text-black pt-3'>
-                        &ldquo;
-                      </div>
-                  
-                  </div>
-                
-                </blockquote>
+              <div className="mb-[-3rem] flex justify-end pt-3 text-7xl text-black">
+                &ldquo;
               </div>
             </div>
-          </section>
+          </blockquote>
+        </div>
+      </div>
+    </section>
   )
 }
