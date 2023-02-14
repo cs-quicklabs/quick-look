976e2e4e420a6865b4b590c2d1146f828a45f139
Ankush <prachi@evabot.ai>
2023-02-14 19:57:11 +0530

    autoclose, hide and types feature added for alert component

--- app/components/Alert/Alert.tsx
+++ app/components/Alert/Alert.tsx
@@ -1,31 +1,72 @@
 import { CheckCircleIcon, XCircleIcon } from '@heroicons/react/solid'
+import { useEffect, useState } from 'react';
 
-export function AlertSuccess(props: any) {
-  const { message } = props
+
+export type AlertType = {
+  message : string | JSX.Element;
+  autoClose : number
+}
+
+export function AlertSuccess(props: AlertType) {
+  const { message, autoClose } = props
+  const [hide, setHide] = useState(true);
+
+  useEffect(() => {
+    if(message){
+      setHide(false)
+    }
+  },[message])
+
+
+  useEffect(() => {
+    if (!hide) {
+      setTimeout(() => {
+        setHide(true);
+      }, autoClose || 4000);
+    }
+  }, [hide, autoClose]);
+  
   return (
-    <div className="rounded-md bg-green-50 p-4" data-cy="alertSuccess">
+    <div className={`rounded-md bg-green-50 p-4 ${hide ? "hidden" : ""}`} data-cy="alertSuccess">
       <div className="flex items-center gap-3">
         <div className="flex-shrink-0">
           <CheckCircleIcon
-            className="h-5 w-5 text-green-800"
+            className="h-5 w-5 text-green-400"
             aria-hidden="true"
           />
         </div>
-        <p className="text-xs font-medium text-green-800">{message}</p>
+        <p className="text-sm font-medium text-green-800">{message}</p>
       </div>
     </div>
   )
 }
 
-export function AlertError(props: any) {
-  const { message } = props
+export function AlertError(props: AlertType) {
+  const { message, autoClose } = props
+  const [hide, setHide] = useState(true);
+
+  useEffect(() => {
+    if(message){
+      setHide(false)
+    }
+  },[message])
+
+
+  useEffect(() => {
+    if (!hide) {
+      setTimeout(() => {
+        setHide(true);
+      }, autoClose || 4000);
+    }
+  }, [hide, autoClose]);
+
   return (
-    <div className="rounded-md bg-red-50 p-4" data-cy="alertError">
+    <div className={`rounded-md bg-red-50 p-4 ${hide ? "hidden" : ""}`} data-cy="alertError">
       <div className="flex items-center gap-3">
         <div className="flex-shrink-0">
           <XCircleIcon className="h-5 w-5 text-red-400" aria-hidden="true" />
         </div>
-        <p className="text-xs font-medium text-red-700">{message}</p>
+        <p className="text-sm font-medium text-red-700">{message}</p>
       </div>
     </div>
   )
