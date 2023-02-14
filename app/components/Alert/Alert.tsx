261a6c5d57daea6bf29ea65d165455443538ab59
Ankush <prachi@evabot.ai>
2023-02-14 19:38:51 +0530

    Error alert component created

--- app/components/Alert/Alert.tsx
+++ app/components/Alert/Alert.tsx
@@ -1,4 +1,4 @@
-import { CheckCircleIcon } from '@heroicons/react/solid'
+import { CheckCircleIcon, XCircleIcon } from '@heroicons/react/solid'
 
 export function AlertSuccess(props: any) {
   const { message } = props
@@ -12,6 +12,20 @@ export function AlertSuccess(props: any) {
           />
         </div>
         <p className="text-xs font-medium text-green-800">{message}</p>
+      </div>
+    </div>
+  )
+}
+
+export function AlertError(props: any) {
+  const { message } = props
+  return (
+    <div className="rounded-md bg-red-50 p-4" data-cy="alertError">
+      <div className="flex items-center gap-3">
+        <div className="flex-shrink-0">
+          <XCircleIcon className="h-5 w-5 text-red-400" aria-hidden="true" />
+        </div>
+        <p className="text-xs font-medium text-red-700">{message}</p>
       </div>
     </div>
   )
