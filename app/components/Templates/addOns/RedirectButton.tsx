66e082b5a3b63e21f0cfeb9f6149ac668278f034
Ankush <prachi@evabot.ai>
2023-03-02 14:38:27 +0530

    new simple template added

---
+++ app/components/Templates/addOns/RedirectButton.tsx
@@ -0,0 +1,17 @@
+export default function RedirectButton({ link, text }: {link:string, text:string}) {
+
+  return (
+    <div className='mt-8 bg-gray-800 flex justify items-center w-full rounded-lg hover:scale-105'>
+      <a
+        href={link}
+        target="_blank"
+        className="text-sm font-semibold leading-5 py-4 w-full text-center text-white" rel="noreferrer"
+      >
+        {text}  
+      </a>
+    </div>
+
+    
+  )
+}
+
