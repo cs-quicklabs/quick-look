de31c29b9973871dbd54b25bda17cd9c52440e93
Aashish Dhawan <aashishdhawan@gmail.com>
2023-04-09 21:47:09 +0530

    fix additional link section

--- app/components/Templates/addOns/AddtionalLinks.tsx
+++ app/components/Templates/addOns/AddtionalLinks.tsx
@@ -13,44 +13,23 @@ export default function AdditionalLinksAddOn({ loaderData }: any) {
           index: number
         ) => (
           // eslint-disable-next-line react/jsx-key
-          <div
-            key={index}
-            className={`flex justify-center items-center ${
-              loaderData?.profileInfo?.templateNumber == '2' ||
-              loaderData?.profileInfo?.templateNumber == '4' ||
-              loaderData?.profileInfo?.templateNumber == '5' ||
-              loaderData?.profileInfo?.templateNumber == '7' ||
-              loaderData?.profileInfo?.templateNumber == '8' ||
-              loaderData?.profileInfo?.templateNumber == '11' ||
-              loaderData?.profileInfo?.templateNumber == '13' ||
-              loaderData?.profileInfo?.templateNumber == '14' ||
-              loaderData?.profileInfo?.templateNumber == '16' ||
-              loaderData?.profileInfo?.templateNumber == '10' ||
-              loaderData?.profileInfo?.templateNumber == '9'
-                ? 'mb-[2rem]'
-                : 'mt-9 mb-4'
-            }`}
-          >
+          <div key={index} className={`flex justify-center items-center mb-4`}>
             <a
-              className=""
+              className={`flex justify-center rounded-md items-center w-[299px] sm:w-[399px] h-10 shadow-sm text-sm leading-5 font-medium text-white ${loaderData?.profile?.additionalLinksColor}`}
               href={
                 additionalSpotlight?.linkUrl.includes('https')
                   ? additionalSpotlight?.linkUrl
                   : 'https://' + additionalSpotlight?.linkUrl
               }
+              style={{
+                backgroundColor: loaderData?.profile?.additionalLinksHexCode,
+                textShadow: '0px 1px black',
+              }}
               target="_blank"
             >
-              <button
-                style={{
-                  backgroundColor: loaderData?.profile?.additionalLinksHexCode,
-                  textShadow: '0px 1px black',
-                }}
-                className={`flex justify-center rounded-md items-center w-[299px] sm:w-[399px] h-10 shadow-sm text-sm leading-5 font-medium text-white ${loaderData?.profile?.additionalLinksColor}`}
-              >
-                {additionalSpotlight?.linkText?.length > 30
-                  ? `${additionalSpotlight?.linkText.substring(0, 30)}...`
-                  : additionalSpotlight?.linkText}
-              </button>
+              {additionalSpotlight?.linkText?.length > 30
+                ? `${additionalSpotlight?.linkText.substring(0, 30)}...`
+                : additionalSpotlight?.linkText}
             </a>
           </div>
         )
