9ff6cf58eb90b9cf80e7c0c723e8a676d6e6c6ae
Aashish Dhawan <aashishdhawan@gmail.com>
2023-04-11 20:44:27 +0530

    refactor banner

--- app/components/Templates/addOns/Banner.tsx
+++ app/components/Templates/addOns/Banner.tsx
@@ -9,24 +9,32 @@ export default function BannerAddOn({ loaderData, mode }: any) {
   const Final = Name.split(' ').join('')
   const bannerColor = loaderData.supportBanner?.bannerColor
   const bannerHex = loaderData.supportBanner?.bannerHex
-  const color = bannerHex ? bannerHex : bannerColor
   const bannerRedirectURL = loaderData?.supportBanner?.bannerlink.includes('https')
     ? loaderData?.supportBanner?.bannerlink
     : 'https://' + loaderData?.supportBanner?.bannerlink
 
   return (
     <div className={`flex items-center justify-center `}>
-      <a
-        href={bannerRedirectURL}
-        target="_blank"
-        className={`flex h-10 hover:underline cursor-pointer items-center justify-center text-sm font-medium leading-5 text-white shadow-sm w-full`}
-        style={{
-          backgroundColor: color,
-        }}
-      >
-        <DynamicHeroIcon icon={Final} Final={Final} />
-        {loaderData?.supportBanner?.bannerText}{' '}
-      </a>
+      {bannerHex ? (
+        <a
+          href={bannerRedirectURL}
+          target="_blank"
+          className={`flex h-10 hover:underline cursor-pointer items-center justify-center text-sm font-medium leading-5 text-white shadow-sm w-full`}
+          style={{ backgroundColor: bannerHex }}
+        >
+          <DynamicHeroIcon icon={Final} Final={Final} />
+          {loaderData?.supportBanner?.bannerText}{' '}
+        </a>
+      ) : (
+        <a
+          href={bannerRedirectURL}
+          target="_blank"
+          className={`${bannerColor} flex h-10 hover:underline cursor-pointer items-center justify-center text-sm font-medium leading-5 text-white shadow-sm w-full`}
+        >
+          <DynamicHeroIcon icon={Final} Final={Final} />
+          {loaderData?.supportBanner?.bannerText}{' '}
+        </a>
+      )}
     </div>
   )
 }
