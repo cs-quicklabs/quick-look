9d54b89f536b990cf13408a8448995c195bee576
Ankush Kumar <ankush1508@gmail.com>
2023-09-21 00:18:30 +0530

    update: remix version - 2.0

--- remix.config.js
+++ remix.config.js
@@ -1,25 +1,20 @@
 const { createRoutesFromFolders } = require('@remix-run/v1-route-convention')
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  serverBuildTarget: 'vercel',
+  // serverBuildTarget: 'vercel',
   // When running locally in development mode, we use the built in remix
   // server. This does not understand the vercel lambda module format,
   // so we default back to the standard build output.
-  server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
+  // server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
   ignoredRouteFiles: ['**/.*'],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "api/index.js",
   // publicPath: "/build/",
   serverDependenciesToBundle: [/^axios.*/],
-  future: {
-    v2_dev: true,
-    v2_routeConvention: true,
-    v2_headers: true,
-    v2_meta: true,
-    v2_errorBoundary: true,
-    v2_normalizeFormMethod: true,
-  },
+  serverModuleFormat: 'cjs',
+  postcss: false,
+  tailwind: false,
   routes(defineRoutes) {
     return createRoutesFromFolders(defineRoutes)
   },
