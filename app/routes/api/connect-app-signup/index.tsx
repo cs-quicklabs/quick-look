b4516eb3d18477dba1b8c4264697ee3f07381a38
Ankush Kumar <ankush1508@gmail.com>
2023-10-19 12:10:58 +0530

    connect-app-signup API and logics

---
+++ app/routes/api/connect-app-signup/index.tsx
@@ -0,0 +1,53 @@
+import type { ActionFunction, LoaderFunction } from '@remix-run/node'
+import { json } from '@remix-run/node'
+import {
+  type connectAppSignUpType,
+  validateConnectAppSignUpRequest,
+  validateSecretKeyAndAppId,
+  connectAppSignUp,
+} from '~/services/auth.service.server'
+
+export const action: ActionFunction = async ({ request }) => {
+  if (request.method.toLowerCase() !== 'post')
+    throw json(
+      {
+        error: 'Bad Request',
+      },
+      { status: 400 }
+    )
+
+  const secretKey = request.headers.get('secret_key') || ''
+  const appId = request.headers.get('app_id') || ''
+  await validateSecretKeyAndAppId({ secretKey, appId })
+
+  const payload = await request
+    .json()
+    .then((v) => v as connectAppSignUpType)
+    .catch(() => {
+      throw json(
+        {
+          error: 'Invalid request. JSON data is missing or empty.',
+        },
+        { status: 400 }
+      )
+    })
+
+  await validateConnectAppSignUpRequest(payload)
+  await connectAppSignUp(payload, appId)
+
+  return json(
+    {
+      success: true,
+    },
+    { status: 201 }
+  )
+}
+
+export const loader: LoaderFunction = async () => {
+  throw json(
+    {
+      error: 'Bad Request',
+    },
+    { status: 400 }
+  )
+}
